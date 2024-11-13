56f2c4689a0e5a1d231bce6f584332e1231c8ffe
pauljons27@gmail.com <141524548+Y3CM@users.noreply.github.com>
2024-11-12 22:25:12 -0500

    ejercicio03

---
+++ Modulo_1/clase03/ejercicio03.java
@@ -0,0 +1,10 @@
+package clase03;
+
+public class ejercicio03 {
+    public static void main(String[] args) {
+        var pecesRojos = 284;
+        var pecesAzules = 163;
+        var totalPeces = pecesRojos + pecesAzules;
+        System.out.println("Total de peces: " + totalPeces);
+    }
+}
