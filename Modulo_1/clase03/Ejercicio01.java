076b4ef056f28f3ab89c972def8c1788d4572bbb
pauljons27@gmail.com <141524548+Y3CM@users.noreply.github.com>
2024-11-12 22:00:57 -0500

    ejercicio01

---
+++ Modulo_1/clase03/Ejercicio01.java
@@ -0,0 +1,11 @@
+package clase03;
+
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        var operacion = 6 / 2 *( 1 + 2 );
+
+        System.out.println("El resultado es: ");
+        System.out.println(operacion);
+        System.out.println("por que al realizar esta opercion primero se resuelve lo que esta en parentesis despues se resuelve segun gerarquia de operaciones y ya que la division y la multiplicacion tienen la misma gerarquia se resuelve de izquierda a derecha");
+    }
+}
