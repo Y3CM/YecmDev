bebd2d1288aa71923124ad3f86eaea5f95a9fd99
pauljons27@gmail.com <141524548+Y3CM@users.noreply.github.com>
2024-11-12 22:22:03 -0500

    ejercicio02

---
+++ Modulo_1/clase03/Ejercicio02.java
@@ -0,0 +1,8 @@
+package clase03;
+
+public class Ejercicio02 {
+    public static void main(String[] args) {
+    var kilo = 1000*5.95/100 ;
+        System.out.println("un kilo del producto cuesta: " + kilo + " euros"); 
+    }
+}
