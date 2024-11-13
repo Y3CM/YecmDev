b7d1d4ce86e14941641d8da6a149b17a2fa25c93
pauljons27@gmail.com <141524548+Y3CM@users.noreply.github.com>
2024-11-12 22:36:35 -0500

    ejercicio04

---
+++ Modulo_1/clase03/ejercicio04.java
@@ -0,0 +1,10 @@
+package clase03;
+
+public class ejercicio04 {
+    public static void main(String[] args) {
+        var saldoInitial = 23;
+        var saldoFinal = 12.75;
+        var gastosTotal = saldoInitial - saldoFinal;
+        System.out.println("Carme se ha gastado " + gastosTotal + " euros en total. }");
+    }
+}
