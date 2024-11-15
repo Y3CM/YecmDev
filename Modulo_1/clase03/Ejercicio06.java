ab46b04c7a160f2298ecdade7bf2842bc60607d5
pauljons27@gmail.com <141524548+Y3CM@users.noreply.github.com>
2024-11-15 09:46:56 -0500

    ejercicio06

---
+++ Modulo_1/clase03/Ejercicio06.java
@@ -0,0 +1,10 @@
+package clase03;
+
+public class Ejercicio06 {
+    public static void main(String[] args) {
+        var precioDesc = 34;
+        var precioTotal = 100 * precioDesc / 85;
+
+        System.out.println("El precio de los pantalones sin el descuento es de " + precioTotal + " euros");
+    }
+}
