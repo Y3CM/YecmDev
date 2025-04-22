55e71d722100d48c18642c29483deb2ca300b929
pauljons27@gmail.com <141524548+Y3CM@users.noreply.github.com>
2025-04-21 21:49:38 -0500

    update: metodos finalización del ejercicio #1

--- Modulo_3/Ejercicio_1/src/App.java
+++ Modulo_3/Ejercicio_1/src/App.java
@@ -4,14 +4,69 @@ public class App {
     public static void main(String[] args) throws Exception {
         Inventario inventario = new Inventario();
          Scanner scan = new Scanner(System.in);
-       System.out.println("Bienvenido al inventario inserta los productos a continuación:");
-        System.out.println("Producto: ");
-        String producto = scan.nextLine();
-        System.out.println("Cantidad: ");
-        int cantidad = scan.nextInt();
-        inventario.agregarProducto(producto, cantidad);
-        inventario.showInventario();
+         boolean salir = false;
+         int opcion;
+         System.out.println("----------------------------------------");
+         System.out.println("Bienvenido a este inventario de productos");
+         System.out.println("----------------------------------------");
+         System.out.println("Por favor elija una de las siguientes opciones:");
+         do {
+            System.out.println("1. Mostrar Inventario");
+            System.out.println("2. Buscar Producto");
+            System.out.println("3. Agregar Producto");
+            System.out.println("4. Eliminar Producto");
+            System.out.println("5. Actualizar stock");
+            System.out.println("6. Salir");
+            opcion = scan.nextInt();
+            scan.nextLine();
+
+            switch (opcion)
+         {
+            case 1:
+                inventario.showInventario();
+                break;
+                case 2:
+                    System.out.println("Ingrese el nombre del producto a buscar: ");
+                    String nombre = scan.nextLine();
+                    inventario.buscarProducto(nombre);
+                break;
+                case 3:
+                System.out.println("Ingrese el nombre del producto a agregar: ");
+                String name = scan.nextLine();
+                System.out.println("Ingrese la cantidad del producto a agregar: ");
+                int cantidad = scan.nextInt();
+                scan.nextLine();
+                inventario.agregarProducto(name, cantidad);
+                break;
+                case 4:
+                inventario.showInventario();
+                System.out.println("Ingrese el nombre del producto a eliminar: ");
+                String producto = scan.nextLine();
+                inventario.eliminarProducto(producto);
+                break;
+                case 5:
+                    System.out.println("ingrese el nombre del producto a actualizar:");
+                    nombre = scan.nextLine();
+                    System.out.println("Ingrese la nueva cantidad del producto: ");
+                    int quanty = scan.nextInt();
+                    inventario.actualizarCantidad(nombre,quanty);
+                break;
+                case 6:
+                System.out.println("Gracias por usar este inventario de productos");
+                salir = true;
+                break;
+                default:
+                System.out.println("Opcion invalida");
+                break;
+         }
+         pressEnter(scan);
+        } while (!salir);
 
         scan.close();
+    }
+
+    private static void pressEnter(Scanner scan){
+        System.out.println("Presione enter para continuar...");
+        scan.nextLine();
     }
 }
--- Modulo_3/Ejercicio_1/src/Inventario.java
+++ Modulo_3/Ejercicio_1/src/Inventario.java
@@ -1,9 +1,11 @@
 import java.util.List;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Inventario {
 
  List<Producto> productos;
+Scanner scan = new Scanner(System.in);
 
 public Inventario(){
     productos = new ArrayList<>();
@@ -11,27 +13,39 @@ public Inventario(){
 
 
 public void showInventario(){
-    for (Producto producto : productos) {
-        System.out.println(producto);
-    }
+   if (!isEmpty(productos)) {
+       for (Producto producto : productos) {
+           System.out.println("----------------------------------------");
+           System.out.println("Productos en el inventario:");
+           System.out.println(producto);
+       }
+   }
+    
 }
 
 public void buscarProducto(String nombre){
-    while (true) {
-        try {
-            for(Producto producto : productos){
-                if(producto.getNombre().equalsIgnoreCase(nombre)){
-                    System.out.println(producto);
-                    break;
-                } else {
+    boolean encontrado = false;
+    if (!isEmpty(productos)) {
+        while (!encontrado) {
+            try {
+                for(Producto producto : productos){
+                    if(producto.getNombre().equalsIgnoreCase(nombre)){
+                        System.out.println(producto);
+                        encontrado = true;
+                        break;
+                    } 
+                }
+                if (!encontrado) {
                     throw new ProductoNoEncontradoException("Producto no encontrado");
                 }
-        }
-    
-        } catch (ProductoNoEncontradoException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace(System.err);
-            System.out.println("Intente nuevamente.");
+        
+            } catch (ProductoNoEncontradoException e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace(System.err);
+                System.out.println("Intente nuevamente.");
+            }
+            System.out.println("Ingrese el nombre del producto a buscar: ");
+            nombre = scan.nextLine();
         }
     }
 }
@@ -56,17 +70,39 @@ try {
 }
 }
 
-public void actualizarCantidad(int cantidad){
+public void actualizarCantidad(String nombre,int cantidad){
     for (Producto producto : productos) {
         try {
-            producto.setCantidad(cantidad);
-            System.out.println("Cantidad actualizada: " + producto);
-            return;
+           if (producto.getNombre().equalsIgnoreCase(nombre)) {
+               if (cantidad > 0) {
+                   producto.setCantidad(cantidad);
+                   System.out.println("Cantidad actualizada: " + producto);
+                   break;
+               } else{
+                   throw new CantidadInvalidaException("La cantidad no puede ser negativa");
+               }
+           } else{
+            throw new ProductoNoEncontradoException("Producto no encontrado");
+           }
+            
+           
         } catch (CantidadInvalidaException e) {
             System.out.println(e.getMessage());
         }
+        catch (ProductoNoEncontradoException e){
+            System.out.println(e.getMessage());
+        }
     }
+    
 
 }
 
+private static boolean isEmpty(List<Producto> productos) {
+    if (productos.isEmpty()) {
+        System.out.println("El inventario esta vacio");
+        return true;
+    } else {
+        return false;
+    }
+}
 }
